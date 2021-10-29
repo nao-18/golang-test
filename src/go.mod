18a060e0e4b47e2ac2bbd4d8eb7f2db0e843d786
nao-18 <kyahyo17@yahoo.co.jp>
2021-10-29 20:08:58 +0900

    add gocheck test

--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,7 @@ module github.com/nao-18/golang-test
 
 go 1.16
 
-require github.com/lib/pq v1.10.3
+require (
+	github.com/lib/pq v1.10.3
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- src/server_test.go
+++ src/server_test.go
@@ -6,23 +6,50 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	. "gopkg.in/check.v1"
 )
 
-func TestHandlePost(t *testing.T) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/post/", handlerRequest(&FakePost{}))
-
-	writer := httptest.NewRecorder()
-	json := strings.NewReader(`{"content": "Create post", "author": "nao"}`)
-	request, _ := http.NewRequest("POST", "/post/", json)
-	mux.ServeHTTP(writer, request)
-
-	if writer.Code != 200 {
-		t.Errorf("Response code is %v", writer.Code)
-	}
+type PostTestSuite struct {
+	mux    *http.ServeMux
+	post   *FakePost
+	writer *httptest.ResponseRecorder
 }
 
-func TestHandleGet(t *testing.T) {
+func init() {
+	Suite(&PostTestSuite{})
+}
+
+func Test(t *testing.T) { TestingT(t) }
+
+func (s *PostTestSuite) SetUpTest(c *C) {
+	s.post = &FakePost{}
+	s.mux = http.NewServeMux()
+	s.mux.HandleFunc("/post/", handlerRequest(s.post))
+	s.writer = httptest.NewRecorder()
+}
+
+func (s *PostTestSuite) TestGetPost(c *C) {
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
+	var post Post
+	json.Unmarshal(s.writer.Body.Bytes(), &post)
+	c.Check(post.Id, Equals, 1)
+}
+
+func (s *PostTestSuite) TestPutPost(c *C) {
+	json := strings.NewReader(`{"content": "Updated post", "author": "nao"}`)
+	request, _ := http.NewRequest("PUT", "/post/1", json)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
+	c.Check(s.post.Id, Equals, 1)
+	c.Check(s.post.Content, Equals, "Updated post")
+}
+
+func (s *PostTestSuite) TestHandleGet(c *C) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/post/", handlerRequest(&FakePost{}))
 
@@ -30,12 +57,20 @@ func TestHandleGet(t *testing.T) {
 	request, _ := http.NewRequest("GET", "/post/1", nil)
 	mux.ServeHTTP(writer, request)
 
-	if writer.Code != 200 {
-		t.Errorf("Response code is %v", writer.Code)
-	}
+	c.Check(writer.Code, Equals, 200)
 	var post Post
 	json.Unmarshal(writer.Body.Bytes(), &post)
-	if post.Id != 1 {
-		t.Errorf("Cannot retrieve JSON post")
-	}
+	c.Check(post.Id, Equals, 1)
+}
+
+func (s *PostTestSuite) TearDownTest(c *C) {
+	c.Log("Finished test - ", c.TestName())
+}
+
+func (s *PostTestSuite) SetUpSuite(c *C) {
+	c.Log("Starting Post Test Suite")
+}
+
+func (s *PostTestSuite) TearDownSuite(c *C) {
+	c.Log("Finishing Post Test Suite")
 }
